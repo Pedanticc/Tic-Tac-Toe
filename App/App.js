fd5706108900420421b5cc834ffb7ed2afdbe19d
Pedanticc <a.m.sakhravi@gmail.com>
2022-11-03 21:50:11 +0330

    Almost there

--- App/App.js
+++ App/App.js
@@ -4,6 +4,7 @@ const allPossibleDirection = [allRow1Boxes, allRow2Boxes, allRow3Boxes,
                               allZ1Boxes, allZ2Boxes]
 
 const checkBoxes = arr => arr.every(v => v === arr[0])
+const sleep = ms => new Promise(r => setTimeout(r, ms))
 
 // --- Event listener for all boxes
 // --- Used the Target algo here. The func knows exactly where to print the symbol. Passed the element as a parameter.
@@ -19,9 +20,26 @@ function printBox(targert){
         else{printing = "O"; XorO = "X"; targert.classList.add("green-box")}
         targert.innerText = printing
     }
+
+    checkStatus()
+
 }
 
 
-// for (let i = 0; i < 3; i++) {
-//     allRow3Boxes[i].style.border = "5px red solid"
-// }
+async function checkStatus(){
+    let result = []
+    for (let i = 0; i < allPossibleDirection.length; i++){
+        for (let j = 0; j < 3; j++){
+            let box = (allPossibleDirection[i])[j].innerText
+            box === "" ? result.push("") : result.push(box)
+        }
+        console.log(result)
+        if ( result.filter((v) => (v === "")).length > 2){ // Filters the arrays or boxes with 3 empty objects ("")
+
+        }
+        else{
+            if (checkBoxes( result )){console.log("win")}
+        }
+        result = []
+    }
+}
